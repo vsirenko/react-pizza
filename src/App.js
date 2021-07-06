3b09269bd73d797f1c6125b176e2e8f0b2c42ae4
Vladimir Sirenko <81309430+V-Sirenko@users.noreply.github.com>
2021-07-06 22:32:46 +0300

    Добавил стили, импортровал лого

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import LogoSvg from './assets/img/pizza-logo.svg'
+
+
 const App = () => {
     return (
         <div className="wrapper">
             <div className="header">
                 <div className="container">
                     <div className="header__logo">
-                        <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo"/>
+                        <img width="38" src={LogoSvg} alt="Pizza logo"/>
                         <div>
                             <h1>React Pizza</h1>
                             <p>самая вкусная пицца во вселенной</p>
